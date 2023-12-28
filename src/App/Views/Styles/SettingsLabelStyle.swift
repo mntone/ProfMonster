d00be17a6170a844fc02e6dffb8c7c5734b70bea
mntone <901816+mntone@users.noreply.github.com>
2023-12-28 11:37:09 +0900

    Changes spacing label to respect OS default.

--- src/App/Views/Styles/SettingsLabelStyle.swift
+++ src/App/Views/Styles/SettingsLabelStyle.swift
@@ -1,18 +1,83 @@
 import SwiftUI
 
+@available(macOS 13.0, *)
+private protocol SettingsLabelMetrics {
+#if os(macOS)
+	var sidebarRowSize: SidebarRowSize { get }
+#endif
+}
+
+@available(macOS 13.0, *)
+extension SettingsLabelMetrics {
+	fileprivate var iconFontSize: CGFloat {
+#if os(macOS)
+		switch sidebarRowSize {
+		case .small:
+			return 9.0
+		case .large:
+			return 21.0
+		case .medium:
+			fallthrough
+		@unknown default:
+			return 15.0
+		}
+#elseif os(watchOS)
+		return 12.0
+#else
+		return 20.0
+#endif
+	}
+
+	fileprivate var iconSize: CGFloat {
+#if os(macOS)
+		switch sidebarRowSize {
+		case .small:
+			return 14.0
+		case .large:
+			return 26.0
+		case .medium:
+			fallthrough
+		@unknown default:
+			return 20.0
+		}
+#elseif os(watchOS)
+		return 16.0
+#else
+		return 30.0
+#endif
+	}
+
+	fileprivate var cornerRadius: CGFloat {
+#if os(macOS)
+		switch sidebarRowSize {
+		case .small:
+			return 3.0
+		case .large:
+			return 6.0
+		case .medium:
+			fallthrough
+		@unknown default:
+			return 4.0
+		}
+#else
+		return 7.0
+#endif
+	}
+}
+
 @available(iOS 16.0, macOS 13.0, watchOS 9.0, *)
-struct SettingsLabelStyle: LabelStyle {
+struct SettingsLabelStyle: LabelStyle, SettingsLabelMetrics {
 	//	@Environment(\.scenePhase)
 	//	private var scenePhase
 
 #if os(macOS)
 	@Environment(\.sidebarRowSize)
-	private var sidebarRowSize
+	fileprivate var sidebarRowSize
 #endif
 
 	@ViewBuilder
 	func makeBody(configuration: Configuration) -> some View {
-		HStack {
+		HStack(spacing: spacing) {
 			let iconSize = self.iconSize
 			configuration.icon
 				.font(.system(size: iconFontSize))
@@ -33,58 +98,20 @@ struct SettingsLabelStyle: LabelStyle {
 		}
 	}
 
-	private var iconFontSize: CGFloat {
+	private var spacing: CGFloat {
 #if os(macOS)
 		switch sidebarRowSize {
-		case .small:
+		case .large:
 			return 9.0
-		case .large:
-			return 21.0
-		case .medium:
+		case .small, .medium:
 			fallthrough
 		@unknown default:
-			return 15.0
+			return 7.0
 		}
 #elseif os(watchOS)
+		return 9.0
+#else
 		return 13.0
-#else
-		return 20.0
-#endif
-	}
-
-	private var iconSize: CGFloat {
-#if os(macOS)
-		switch sidebarRowSize {
-		case .small:
-			return 14.0
-		case .large:
-			return 26.0
-		case .medium:
-			fallthrough
-		@unknown default:
-			return 20.0
-		}
-#elseif os(watchOS)
-		return 16.0
-#else
-		return 30.0
-#endif
-	}
-
-	private var cornerRadius: CGFloat {
-#if os(macOS)
-		switch sidebarRowSize {
-		case .small:
-			return 3.0
-		case .large:
-			return 6.0
-		case .medium:
-			fallthrough
-		@unknown default:
-			return 4.0
-		}
-#else
-		return 7.0
 #endif
 	}
 }
@@ -101,10 +128,15 @@ extension LabelStyle where Self == SettingsLabelStyle {
 @available(iOS, introduced: 15.0, deprecated: 16.0, message: "Use SettingsLabelStyle instead")
 @available(watchOS, introduced: 8.0, deprecated: 9.0, message: "Use SettingsLabelStyle instead")
 @available(macOS, unavailable)
-struct SettingsLabelStyleBackport: LabelStyle {
+struct SettingsLabelStyleBackport: LabelStyle, SettingsLabelMetrics {
+#if os(watchOS)
+	@Environment(\.watchMetrics)
+	private var watchMetrics
+#endif
+
 	@ViewBuilder
 	func makeBody(configuration: Configuration) -> some View {
-		HStack {
+		HStack(spacing: spacing) {
 			let iconSize = self.iconSize
 			configuration.icon
 				.font(.system(size: iconFontSize))
@@ -120,27 +152,17 @@ struct SettingsLabelStyleBackport: LabelStyle {
 		}
 	}
 
-	private var iconFontSize: CGFloat {
+	private var spacing: CGFloat {
 #if os(watchOS)
+		if watchMetrics.device == .case38 {
+			return 8.0
+		} else {
+			return 9.0
+		}
+#else
 		return 13.0
-#else
-		return 20.0
 #endif
 	}
-
-	private var iconSize: CGFloat {
-#if os(watchOS)
-		return 16.0
-#else
-		return 30.0
-#endif
-	}
-
-#if os(iOS)
-	private var cornerRadius: CGFloat {
-		return 7.0
-	}
-#endif
 }
 
 @available(iOS, introduced: 15.0, deprecated: 16.0, message: "Use LabelStyle.settings instead")
