85470eb95d4a95d4bc988385c6d6eba6832eed5e
mntone <901816+mntone@users.noreply.github.com>
2024-01-06 07:30:34 +0900

    Mock is now loaded from Bundle JSON.

---
+++ src/Core/DataSources/MockData.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public enum MockMonsterKey: String, CaseIterable {
+	case buchaCat = "bucha_cat"
+	case guluQoo = "gulu_qoo"
+}
+
+public enum MockData {
+	public static func monster(_ key: MockMonsterKey) -> MHMonster? {
+		let bundle = Bundle(for: NetworkDataSource.self)
+		guard let url = bundle.url(forResource: key.rawValue, withExtension: "json"),
+			  let data = try? Data(contentsOf: url),
+			  let json = try? JSONDecoder().decode(MHMonster.self, from: data) else {
+			return nil
+		}
+		return json
+	}
+
+	public static func physiology(_ key: MockMonsterKey) -> Physiologies? {
+		guard let monster = Self.monster(key) else {
+			return nil
+		}
+
+		let mapper = PhysiologyMapper(languageService: PassthroughtLanguageService())
+		return mapper.map(json: monster, options: PhysiologyMapperOptions(mergeParts: false))
+	}
+
+}
--- src/Core/DataSources/MockDataSource.swift
+++ src/Core/DataSources/MockDataSource.swift
@@ -12,9 +12,24 @@ public struct MockDataSource {
 
 	static let config = MHConfig(version: 2, titles: [configTitle])
 
-	static let localization = MHLocalization(monsters: [
+	static let localizationEnglish = MHLocalization(monsters: [
 		MHLocalizationMonster(id: "gulu_qoo",
 							  name: "Gulu Qoo",
+							  anotherName: nil,
+							  keywords: []),
+		MHLocalizationMonster(id: "bucha_cat",
+							  name: "Bucha Cat",
+							  anotherName: nil,
+							  keywords: [])
+	], states: [:])
+
+	static let localizationJapanese = MHLocalization(monsters: [
+		MHLocalizationMonster(id: "gulu_qoo",
+							  name: "グークー",
+							  anotherName: nil,
+							  keywords: []),
+		MHLocalizationMonster(id: "bucha_cat",
+							  name: "ブチャネコ",
 							  anotherName: nil,
 							  keywords: [])
 	], states: [:])
@@ -23,111 +38,10 @@ public struct MockDataSource {
 							 localization: ["en"],
 							 monsters: [MHGameMonster(id: "gulu_qoo", type: "piyopiyo")])
 
-	public static let monster1 = MHMonster(id: "gulu_qoo",
-										   type: "piyopiyo",
-										   version: "1.0.0",
-										   physiologies: [
-			MHMonsterPhysiology(
-			   parts: ["head"],
-			   values: [
-				   MHMonsterPhysiologyValue(
-					   states: ["default"],
-					   slash: 68,
-					   strike: 68,
-					   shell: 60,
-					   fire: 15,
-					   water: 20,
-					   thunder: 15,
-					   ice: 15,
-					   dragon: 15,
-					   stun: 100),
-			   ]
-		   ),
-		   MHMonsterPhysiology(
-			   parts: ["torso"],
-			   values: [
-				   MHMonsterPhysiologyValue(
-					   states: ["default"],
-					   slash: 45,
-					   strike: 45,
-					   shell: 40,
-					   fire: 15,
-					   water: 20,
-					   thunder: 15,
-					   ice: 15,
-					   dragon: 15,
-					   stun: 0),
-			   ]
-		   ),
-		   MHMonsterPhysiology(
-			   parts: ["foreleg"],
-			   values: [
-				   MHMonsterPhysiologyValue(
-					   states: ["default"],
-					   slash: 52,
-					   strike: 56,
-					   shell: 45,
-					   fire: 15,
-					   water: 20,
-					   thunder: 15,
-					   ice: 15,
-					   dragon: 10,
-					   stun: 0),
-			   ]
-		   ),
-		   MHMonsterPhysiology(
-			   parts: ["tail"],
-			   values: [
-				   MHMonsterPhysiologyValue(
-					   states: ["default"],
-					   slash: 55,
-					   strike: 50,
-					   shell: 35,
-					   fire: 15,
-					   water: 20,
-					   thunder: 15,
-					   ice: 15,
-					   dragon: 15,
-					   stun: 0),
-			   ]
-		   ),
-		   MHMonsterPhysiology(
-			   parts: ["boulder"],
-			   values: [
-				   MHMonsterPhysiologyValue(
-					   states: ["default"],
-					   slash: 5,
-					   strike: 5,
-					   shell: 5,
-					   fire: 5,
-					   water: 20,
-					   thunder: 5,
-					   ice: 5,
-					   dragon: 5,
-					   stun: 0),
-			   ]
-		   ),
-		   MHMonsterPhysiology(
-			   parts: ["pod"],
-			   values: [
-				   MHMonsterPhysiologyValue(
-					   states: ["default"],
-					   slash: 50,
-					   strike: 50,
-					   shell: 50,
-					   fire: 15,
-					   water: 20,
-					   thunder: 15,
-					   ice: 15,
-					   dragon: 15,
-					   stun: 0),
-			   ]
-		   ),
-	   ])
-
 	public static var physiology1: Physiologies {
+		let monster = MockData.monster(.guluQoo)!
 		let mapper = PhysiologyMapper(languageService: PassthroughtLanguageService())
-		return mapper.map(json: monster1, options: PhysiologyMapperOptions(mergeParts: false))
+		return mapper.map(json: monster, options: PhysiologyMapperOptions(mergeParts: false))
 	}
 
 	public init() {
@@ -146,10 +60,18 @@ extension MockDataSource: DataSource {
 	}
 
 	func getLocalization(of key: String, for titleId: String) async throws -> MHLocalization {
-		Self.localization
+		if key == "ja" {
+			return Self.localizationJapanese
+		} else {
+			return Self.localizationEnglish
+		}
 	}
 
 	func getMonster(of id: String, for titleId: String) async throws -> MHMonster {
-		Self.monster1
+		guard let key = MockMonsterKey(rawValue: id),
+			  let mock = MockData.monster(key) else {
+			throw StarSwingsError.notExists
+		}
+		return mock
 	}
 }
