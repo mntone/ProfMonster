ceaf3aeb731acac3abb602f631239b83a1abdbf3
mntone <901816+mntone@users.noreply.github.com>
2024-01-10 22:22:53 +0900

    Fixes an issue of ci_post_xcodebuild.sh.

--- src/ci_scripts/ci_post_xcodebuild.sh
+++ src/ci_scripts/ci_post_xcodebuild.sh
@@ -16,7 +16,7 @@ getSection() {
 	if [ "$END" = "" ]; then
 		END=$(($TAIL_LINE + 1))
 	fi
-	local RESULT=$(printf '%s' $RELEASE_NOTE_T0 | head -n $(($START + $END - 1)) | tail -n $END | perl -pe "s/^## <!--$1-->(.+?)$/[\$1]/")
+	local RESULT=$(printf '%s' $2 | head -n $(($START + $END - 1)) | tail -n $END | perl -pe "s/^## <!--$1-->(.+?)$/[\$1]/")
 	echo "$RESULT"
 }
 
@@ -28,11 +28,11 @@ MESSAGE=`getSection "MESSAGE" "$RELEASE_NOTE_T0" | grep -v '^[[:space:]]*$'`
 KNOWN_ISSUES=`getSection "KNOWN_ISSUES" "$RELEASE_NOTE_T0" "YES" | perl -pe 's/^###\s+(.+?)$/$1/'`
 
 VERSION_1=$(printf '%s' "$LATEST_RELEASES" | jq -r '.[1].name')
-RELEASE_NOTE_T1=$(printf '%s' "$LATEST_RELEASES" | jq -r '.[1].body')
+RELEASE_NOTE_T1=$(printf '%s' "$LATEST_RELEASES" | jq -r '.[1].body' | tr -d '\r')
 RELEASE_NOTE_1=$(printf '%s' $RELEASE_NOTE_T1 | head -n $(($(printf '%s' $RELEASE_NOTE_T1 | grep -n '## ' | head -n 1 | cut -d ':' -f1) - 2)) | perl -pe 's/\b([a-f0-9]{7})[a-f0-9]{33}\b/$1/ge')
 
 VERSION_2=$(printf '%s' "$LATEST_RELEASES" | jq -r '.[2].name')
-RELEASE_NOTE_T2=$(printf '%s' "$LATEST_RELEASES" | jq -r '.[2].body')
+RELEASE_NOTE_T2=$(printf '%s' "$LATEST_RELEASES" | jq -r '.[2].body' | tr -d '\r')
 RELEASE_NOTE_2=$(printf '%s' $RELEASE_NOTE_T2 | head -n $(($(printf '%s' $RELEASE_NOTE_T2 | grep -n '## ' | head -n 1 | cut -d ':' -f1) - 2)) | perl -pe 's/\b([a-f0-9]{7})[a-f0-9]{33}\b/$1/ge')
 
 RELEASE_NOTES=$(cat << EOS
