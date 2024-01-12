7aa0449563e736c32cf6d3fbc3ab4611a57906f2
mntone <901816+mntone@users.noreply.github.com>
2024-01-12 17:13:38 +0900

    Improves loading indicator UI.

--- src/App/Views/Modifiers/StateOverlayModifier.swift
+++ src/App/Views/Modifiers/StateOverlayModifier.swift
@@ -1,5 +1,51 @@
 import SwiftUI
 
+#if !os(macOS)
+
+@available(macOS, unavailable)
+private struct ProgressIndicatorView: View {
+	var body: some View {
+		Color.clear
+		VStack(spacing: 10) {
+			ProgressView()
+			Text("Loading…")
+		}
+	}
+}
+
+#endif
+
+#if !os(watchOS)
+
+@available(watchOS, unavailable)
+private struct DelayedProgressIndicatorView: View {
+	@State
+	private var isProgressShown: Bool = false
+
+	var body: some View {
+		Color.clear
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.task {
+				try? await Task.sleep(nanoseconds: 666_666_666 /* 2/3 secs */)
+				isProgressShown = true
+			}
+
+		if isProgressShown {
+			Color(white: 0, opacity: 0.2)
+				.frame(maxWidth: .infinity, maxHeight: .infinity)
+			VStack(spacing: 20) {
+				ProgressView()
+				Text("Loading…")
+			}
+			.offset(y: 10)
+			.frame(width: 120, height: 120)
+			.background(.thinMaterial, in: RoundedRectangle(cornerRadius: 8))
+		}
+	}
+}
+
+#endif
+
 struct StateOverlayModifier: ViewModifier {
 	let state: RequestState
 
@@ -7,12 +53,22 @@ struct StateOverlayModifier: ViewModifier {
 		content.overlay {
 			switch state {
 			case .loading:
-				ProgressView()
+#if os(iOS)
+				if UIDevice.current.userInterfaceIdiom == .pad {
+					DelayedProgressIndicatorView()
+				} else {
+					ProgressIndicatorView()
+				}
+#elseif os(macOS)
+				DelayedProgressIndicatorView()
+#else
+				ProgressIndicatorView()
+#endif
 			case let .failure(_, error):
 				Text(error.label)
 					.scenePadding()
 			default:
-				EmptyView()
+				Never?.none
 			}
 		}
 	}
